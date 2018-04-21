67a9c03d0a86e1d89eea7c008d479d193361e0b7
aerisDoesCodes <aerisdoescodes@gmail.com>
2018-04-21 13:41:25 +0800

    Added - locales (ph.js)

---
+++ locales/ph.js
@@ -0,0 +1,20 @@
+{
+	"Hi! I am a bot for DISBOARD (<https://disboard.org/>)": "Hi! Ako ay isang bot para sa DISBOARD (<https://disboard.org/>)",
+	"COMMAND LIST": "Listahan ng Commands",
+	"This!": "Ito!",
+	"Bump this server": "Bump ang server na ito",
+	"Change Instant Invite to this channel": "Baguhin ang Instant Invite sa channel na ito",
+	"Bump done": "Bump done",
+	"Please wait another %s minute": {
+		"one": "Maghintay ng %s minuto",
+		"other": "Maghintay ng %s minuto"
+	},
+	"Refresh done": "Tapos na ang refresh",
+	"Please give me Create Instant Invite permission to me": "Paki bigyan ako ng pahintulot na Gumawa ng \`Create Instant\`",
+	"And then send this command: `!disboard refresh`": "Pagkatapos i-send ang utos na ito: `!disboard refresh`",
+	"Your server have not been added to DISBOARD (<https://disboard.org/>) :tired_face:": "Ang iyong server ay hinda pa idinagdag sa DISBOARD (<https://disboard.org/>) :tired_face:",
+	"Please add one to the site before using this bot!": "Mangyaring magdagdag ng isa sa site bago gamitin ang bot na ito!",
+	"Invite link has been changed": "Ang invite link ay nag bago",
+	"I couldn't bump your server because it's private": "Hindi ko mai-bump ang iyong server dahil pribado ito",
+	"Edit your server at Dashboard: https://disboard.org/dashboard": "I-edit ang iyong server sa Dashboard: https://disboard.org/dashboard"
+}
