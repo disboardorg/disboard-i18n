f8a47a21fad9fc2d2dad133f70511c516c3dd0aa
Austin Huang <austinhuang0131@gmail.com>
2018-09-29 21:14:29 -0400

    Update zh-cn.js

--- locales/zh-cn.js
+++ locales/zh-cn.js
@@ -5,10 +5,35 @@
 	"Bump this server": "在服务器列表上顶本服务器",
 	"Bump done": "服务器已顶",
 	"Please wait another %s minute": {
-		"one": "再等%s分钟！",
-		"other": "再等%s分钟！"
+		"one": "请再等%s分钟！",
+		"other": "请再等%s分钟！"
+	},
+	"Please wait another **%s** minute": {
+		"one": "请再等**%s**分钟！",
+		"other": "请再等**%s**分钟！"
 	},
 	"Refresh done": "服务器信息已更新。",
-	"Please give me Create Instant Invite permission to me": "我需要“建立即时邀请”的权限！",
-	"And then send this command: `!disboard refresh`": "And then send this command: `!disboard refresh`"
+	"Please give me Create Instant Invite permission to me": "请给我“建立即时邀请”的权限",
+	"And then send this command: `!disboard refresh`": "然后输入 `!disboard refresh`",
+	"Your server have not been added to DISBOARD (<https://disboard.org/>) :tired_face:": "您的服务器还未被添加到DISBOARD (<https://disboard.org/>) 上 :tired_face:",
+	"Please add one to the site before using this bot!": "请在网站上添加服务器后方可使用本机器人！",
+	"Change Instant Invite to this channel": "更改默认邀请至所在的频道",
+	"I couldn't bump your server because it's private": "因为您的服务器是私密的，我不能顶您的服务器！",
+	"Edit your server at Dashboard: https://disboard.org/dashboard": "在设置中更改服务器设定: https://disboard.org/dashboard",
+	"Invite link has been changed": "邀请链接已更改",
+	"Check it on DISBOARD": "在 DISBOARD 上查看",
+	"DISBOARD: The Public Server List": "DISBOARD: 公共服务器列表",
+	"Server ID": "服务器 ID",
+	"Default channel": "默认频道",
+	"Command alias": "命令前缀",
+	"Shard ID": "Shard ID",
+	"Main Language": "服务器语言",
+	"Not set": "未设置",
+	"Return the server page URL on DISBOARD": "显示您的服务器在 DISBOARD 的页面URL",
+	"Usage: `!disboard alias <alias>`": "使用方法： `!disboard alias <alias>`",
+	"Set the command alias to *<alias>*": "将命令前缀设置为 *<alias>*",
+	"The command alias has been changed to **`%s`**": "服务器前缀已改为 **`%s`**",
+	"The command alias length should be less than %d": "服务器前缀长度应少于 %d 字",
+	"Admin only": "仅限管理员",
+	"This command can only be executed by administrators": "只有管理员才能运行该命令"
 }
