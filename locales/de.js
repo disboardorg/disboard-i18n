5cccd012c12c886f35998bd52fb60f3bcd3c43d6
Greg TK <33130539+GregTCLTK@users.noreply.github.com>
2019-06-02 17:13:06 +0200

    Update de.js

--- locales/de.js
+++ locales/de.js
@@ -5,11 +5,11 @@
   "This!": "Das!",
   "Bump this server": "Bumpe diesen Server",
   "Get server page link": "Gibt die Serverseite URL auf DISBOARD wieder",
-  "Change Instant Invite to this channel": "Ändert die Soforteinladung zu diesem Channel",
+  "Change Instant Invite to this channel": "Ändert den Soforteinladungslink zu diesem Channel",
   "Admin only": "Nur Administratoren",
   "Bump done": "Bump erfolgreich",
   "Check it on DISBOARD": "Sehe es auf DISBOARD",
-  "Please give me `Create Instant Invite` permission on this channel": "Bitte gib mir die Berechtigung, Einladungen zu erstellen.",
+  "Please give me `Create Instant Invite` permission on this channel": "Bitte gib mir die Berechtigung in diesem Kanal, Einladungen zu erstellen.",
   "And then send this command: `%s`": "Nutze dann den Befehl: `%s`",
   "Send this command to create invite: `%s`": "Senden Sie diesen Befehl, um die Einladung zu erstellen: `%s`",
 	"Invite link has been changed": "Soforteinladungslink wurde geändert",
@@ -19,7 +19,7 @@
 	"Default channel": "Standard Channel",
 	"Invite Link": "Soforteinladungslink",
   "Main Language": "Serversprache",
-	"I'm handling your command": "Ich handle mit deinem Befehl",
-  "This server is set to **private**. \nSet to public from the dashboard (https://disboard.org/dashboard) to bump this server": "Dieser Server ist auf privat gesetzt. \nLegen Sie über das Dashboard (https://disboard.org/dashboard) den Eintrag public fest, um diesen Server hochzuladen",
-  "This server has not been added to DISBOARD yet. \nAdd this server from the dashboard (https://disboard.org/dashboard)": "Dieser Server wurde auf DISBOARD noch nicht hinzugefügt. \nDiesen Server über das Dashboard hinzufügen (https://disboard.org/dashboard)"
+	"I'm handling your command": "Ich verarbeite deinen Befehl",
+  "This server is set to **private**. \nSet to public from the dashboard (https://disboard.org/dashboard) to bump this server": "Dieser Server ist auf privat gesetzt. \nLegen Sie über das Dashboard (https://disboard.org/dashboard) den Eintrag auf öffentlich fest, um diesen Server zu bumpen",
+  "This server has not been added to DISBOARD yet. \nAdd this server from the dashboard (https://disboard.org/dashboard)": "Dieser Server wurde auf DISBOARD noch nicht hinzugefügt. \nFüge diesen Server über das Dashboard hinzufügen (https://disboard.org/dashboard)"
 }
