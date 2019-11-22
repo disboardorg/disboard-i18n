20b59db8f525140139b4fb7a19906cd2c97d3499
Austin Huang <im@austinhuang.me>
2019-11-22 10:48:38 -0500

    Update tr.js

--- locales/tr.js
+++ locales/tr.js
@@ -7,14 +7,14 @@
     "Get server page link": "Sunucu sayfa linkini al",
     "Change Instant Invite to this channel. If [channel] is specified, create Instant Invite for that channel": "Anlik daveti bu kanal icin degistir. Eger [kanal] belirtilmemisse, bu kanal icin anlik davet olusturun",
     "Admin only": "Sadece yonetici",
-    "Bump done": "Patlatma tamamland˝",
-    "Check it on DISBOARD": "DISBOARD ¸zerinden kontrol et",
+    "Bump done": "Patlatma tamamlandi",
+    "Check it on DISBOARD": "DISBOARD uzerinden kontrol et",
     "Please give me Create Instant Invite permission on this channel": "Lutfen bu kanal uzerinde 'Anlik Davet Olustur' yetkisi ver"
     "And then send this command: %s": "And then send this command: %s",
     "Send this command to create invite: %s": "Ve ondan sonra bu komutu gonder: %s",
     "Invite link has been changed": "Davet linki degistirildi",
     "Invalid channel: %s": "Gecersiz kanal: %s",
-    "This command can only be executed by administrators": "Bu komut sadece yˆneticiler taraf˝ndan kullan˝labilir",
+    "This command can only be executed by administrators": "Bu komut sadece yˆneticiler tarafindan kullanilabilir",
     "Server ID": "Sunucu Kimligi",
     "Shard ID": "Shard Kimligi",
     "Default channel": "Normal kanal",
@@ -22,5 +22,5 @@
     "Main Language": "Ana Dil",
     "I'm handling your command": "Komudunu ele aliyorum.",
     "This server is set to private. \nSet to public from the dashboard (https://disboard.org/dashboard) to bump this server": "Bu sunucu **ozel** olarak ayarlanmistir. \nKontrol panelinden (https://disboard.org/dashboard) sunucu patlatmak icin herkese acik hale getirin.",
-    "This server has not been added to DISBOARD yet. \nAdd this server from the dashboard (https://disboard.org/dashboard)": "Bu sunucu DISBOARD'a hen¸z eklenmedi. Kontrol panelinden bu sunucuyu ekle."
+    "This server has not been added to DISBOARD yet. \nAdd this server from the dashboard (https://disboard.org/dashboard)": "Bu sunucu DISBOARD'a henuz eklenmedi. Kontrol panelinden bu sunucuyu ekle."
 }
