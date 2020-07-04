1d7be650f640a48c8759559807c5e553260bb77f
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2020-07-04 08:55:31 +0200

    Update de.js
    Changed a few minor things.

--- locales/de.js
+++ locales/de.js
@@ -9,7 +9,7 @@
   "Admin only": "Nur für Administratoren",
   "Bump done": "Bump erfolgreich",
   "Check it on DISBOARD": "Sieh es auf DISBOARD",
-  "Please give me `Create Instant Invite` permission on this channel": "Bitte gib mir die Berechtigung, in diesem Kanal Einladungen zu erstellen.",
+  "Please give me `Create Instant Invite` permission on this channel": "Bitte gib mir die `Einladung erstellen` Berechtigung in diesem Kanal",
   "Please give me `Manage Channels` permission in order to receive invite change event": "Bitte gib mir die `Kanäle verwalten` Berechtigung, um benachrichtigt zu werden, wenn der Einladungslink geändert wird",
   "And then send this command: `%s`": "Nutze dann diesen Befehl: `%s`",
   "Send this command to create invite: `%s`": "Nutze diesen Befehl, um einen Einladungslink zu erstellen: `%s`",
@@ -24,10 +24,10 @@
   "Invite Link": "Einladungslink",
   "Main Language": "Serversprache",
   "I'm handling your command": "Ich verarbeite deinen Befehl",
-  "This server is set to **private**. \nSet to public from the dashboard (https://disboard.org/dashboard) to bump this server": "Dieser Server ist auf **Privat** gestellt. \nStelle den Server über des Dashboard(https://disboard.org/dashboard) auf Öffentlich um, damit du ihn bumpen kannst.",
+  "This server is set to **private**. \nSet to public from the dashboard (https://disboard.org/dashboard) to bump this server": "Dieser Server ist auf **Privat** gestellt. \nStelle den Server über das Dashboard(https://disboard.org/dashboard) auf Öffentlich um, damit du ihn bumpen kannst.",
   "This server has not been added to DISBOARD yet. \nAdd this server from the dashboard (https://disboard.org/dashboard)": "Dieser Server wurde noch nicht zu DISBOARD hinzugefügt. \nFüge ihn über das Dashboard (https://disboard.org/dashboard) hinzu",
   "This server is not listed because the invite has been deleted": "Dieser Server ist nicht gelistet, da die Einladung gelöscht worden ist",
   "Create invite with `%s` command": "Erstelle eine Einladung mit dem Befehl `%s`",
-  "Command failed": "Der Befehl war erfolglos",
+  "Command failed": "Der Befehl ist fehlgeschlagen",
   "DISBOARD API server seems to be down": "Der DISBOARD API Server scheint außer Betrieb zu sein"
 }
