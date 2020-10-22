af4972edcf6b933093a70bb9761e5aa0ae2c048c
synarvis <45217233+synarvis@users.noreply.github.com>
2020-10-22 20:09:34 +0200

    Update guidelines-de.php

--- guidelines/guidelines-de.php
+++ guidelines/guidelines-de.php
@@ -10,12 +10,12 @@ $this->title = Yii::t('app', 'Richtlinien');
     Zuletzt geändert: 2020-04-10
 </p>
 <p>
-    Um eine sichere Gemeinschaft zu erhalten, gibt es einige Rechtlinien, die von dir befolgt werden müssen <br>
+    Um eine sichere Gemeinschaft zu gewährleisten, gibt es einige Rechtlinien, die von dir befolgt werden müssen <br>
     Durch die Verwendung von DISBOARD erklärst du dich mit folgenden Richtlinien einverstanden:
 </p>
 <ul>
     <li>
-        Die Verwendung von Schimpfwörtern oder NSFW (sexueller Inhalt) nach unserem Ermessen in der Meta (Titel, Beschreibung und Bild), Rezension oder anderen Inhalten eines Servers, die von anderen Benutzern auf DISBOARD gesehen werden können, ist nicht erlaubt. Dies führt zur Entfernung des Inhalts, du hast jedoch die Möglichkeit, den Inhalt in einer angemessenen Sprache neu zu posten.
+        Die Verwendung von Schimpfwörtern, Flüchen oder NSFW (sexueller Inhalt) nach unserem Ermessen in der Meta (Titel, Beschreibung und Bild), Rezension oder anderen Inhalten eines Servers, welche von anderen Benutzern auf DISBOARD gesehen werden können, ist nicht erlaubt. Dies führt zur Entfernung des Inhalts, jedoch hast du die Möglichkeit, den Inhalt in einer angemessenen Sprache neu zu veröffentlichen.
     </li>
     <li>
         Du darfst keine Gesetze oder Vorschriften in deinem Aufenthaltsland verletzen oder deren Verletzung fördern.
@@ -24,13 +24,13 @@ $this->title = Yii::t('app', 'Richtlinien');
         Server, die gegen die <a href="https://discord.com/guidelines" target="_blank">Discord Community Richtlinien</a> verletzen, sind nicht erlaubt.
     </li>
     <li>
-        Die Verwendung von Bots oder anderen Skripten zur automatischen Ausführung von Aktionen auf DISBOARD, wie z.B. das Bumpen eines Servers ("auto-bump") ist nicht erlaubt. Bumpen, schreiben von Rezensionen usw. muss manuell getan werden.
+        Die Verwendung von Bots oder anderen Skripten zur automatischen Ausführung von Aktionen auf DISBOARD, wie z.B. das Bumpen eines Servers ("auto-bump") ist nicht erlaubt. Bumpen, das Schreiben von Rezensionen usw. muss manuell getan werden.
     </li>
     <li>
         Du darfst keine Server hinzufügen, die nur der Umleitung oder Werbung eines anderen Servers dienen oder keinen Inhalt haben (nach unserem Ermessen).
     </li>
     <li>
-        Du darfst Nutzer nicht belohnen oder zwingen, Aktionen auf DISBOARD durchzuführen. Beispielsweise darfst du Nutzer nicht dafür belohnen eine nette Rezension auf einem Server zu schreiben, oder sie zwingen, einen Server zu bumpen.
+        Du darfst Nutzer nicht belohnen oder zwingen, Aktionen auf DISBOARD durchzuführen. Beispielsweise darfst du Nutzer nicht dafür belohnen, eine positive Rezension auf einem Server zu schreiben, oder sie zwingen, einen Server zu bumpen.
     </li>
     <li>
         Du darfst nicht mehrere Discord-Konten erstellen, um mehrere Rezensionen zu schreiben. Bitte schreibe nur eine Rezension pro Person.
