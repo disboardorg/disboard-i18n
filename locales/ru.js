f06d11cdf0c225360a869d53a71f7a67dd393bff
Antsiferov Andrew <summet.dev@gmail.com>
2020-11-05 00:52:30 +0300

    Several fixes

--- locales/ru.js
+++ locales/ru.js
@@ -31,6 +31,6 @@
   "Command failed": "Не удалось выполнить команду",
   "DISBOARD API server seems to be down": "Кажется, у серверов DISBOARD API неполадки",
   "You are rate limited. Please wait %s seconds": "Превышен лимит. Пожалуйста, подождите %s с.",
-  "**How do I add my server to DISBOARD?**\n\n 1. Login on DISBOARD website\n 2. Go to Dashboard\n 3. Click \"Add new server\"\nFill out your server info and save it. You will be redirected to Discord's authorization screen. If not, click the \"Add Bot\" button on the server edit page.": "**How do I add my server to DISBOARD?**\n\n 1. Login on DISBOARD website\n 2. Go to Dashboard\n 3. Click \"Add new server\"\nFill out your server info and save it. You will be redirected to Discord's authorization screen. If not, click the \"Add Bot\" button on the server edit page.",
+  "**How do I add my server to DISBOARD?**\n\n 1. Login on DISBOARD website\n 2. Go to Dashboard\n 3. Click \"Add new server\"\nFill out your server info and save it. You will be redirected to Discord's authorization screen. If not, click the \"Add Bot\" button on the server edit page.": "**Как мне добавить свой сервер на DISBOARD?**\n\n 1. Авторизуйтесь на сайте DISBOARD.\n 2. \n 3. Нажмите кнопку \"Добавить новый сервер\".\nЗаполните всю информацию о сервере и сохраните. Вы будете перенаправлены на сайт авторизации Discord. Если же нет, то нажмите на кнопку "Добавить бота".",
   "Support server: <%s>": "Сервер поддержки: <%s>"
 }
