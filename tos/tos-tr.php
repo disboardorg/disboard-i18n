6622bc1f2cf502b675c03f941767524ed4cb7be3
Thrasher <ajtroag@gmail.com>
2025-03-22 06:49:36 +0300

    Add Turkish Translation for TOS

---
+++ tos/tos-tr.php
@@ -0,0 +1,151 @@
+<?php
+use yii\helpers\Url;
+
+$this->title = 'Kullanım Koşulları';
+?>
+<h1>
+    <?= $this->title ?>
+</h1>
+<p class="has-text-right is-italic">
+    Son Güncelleme: 2020-7-27
+</p>
+<h2>Giriş</h2>
+<p class="tos-summary">
+    Burası DISBOARD. Web sitemizi kullanarak, aşağıdaki koşulları kabul etmiş olursunuz.
+</p>
+<ul>
+    <li>
+        Aşağıdaki Kullanım Koşulları (bundan sonra "Koşullar" olarak anılacaktır), DISBOARD tarafından sağlanan hizmetimizin (bundan sonra "Hizmet" olarak anılacaktır) uygun kullanımını tanımlayan şartları belirtir. (bundan sonra "biz", "bize", "bizim" olarak anılacaktır).
+    </li>
+    <li>
+        Bu koşullar, DISBOARD web sitesinin kullanım şartları olarak tanımlanmıştır. DISBOARD web sitesini kullanarak, kullanıcılar bu koşullara uymayı kabul ederler.
+    </li>
+    <li>
+        DISBOARD web sitesini kullanmak, kullanıcıların bu koşulları okuduğunu ve kabul ettiğini gösterir; kullanıcıların bulundukları yargı bölgesinde reşit olmaları veya reşit değillerse yasal vasilerinin bu koşulları incelemiş ve kabul etmiş olmaları şartıyla.
+    </li>
+</ul>
+
+<h2>Değişiklikler</h2>
+<p class="tos-summary">Bu koşulları istediğimiz zaman değiştirebiliriz, bu yüzden düzenli olarak kontrol etmelisiniz.</p>
+<ul>
+    <li>
+        Bu Koşulları herhangi bir kullanıcıya bildirimde bulunarak veya bulunmadan istediğimiz zaman revize edebilir ve güncelleyebiliriz.
+    </li>
+    <li>
+        Koşullardaki değişiklikler, aksi belirtilmedikçe DISBOARD web sitesinde güncellendiğinde geçerli olacaktır.
+    </li>
+    <li>
+        Kullanıcı, bu Koşullarda herhangi bir değişiklik yapıldıktan sonra Hizmeti kullanmaya devam ederse, bu Koşulların en yeni sürümüyle bağlı olacaktır.
+    </li>
+</ul>
+</p>
+
+<h2>Gizlilik Politikası</h2>
+<p class="tos-summary">
+Bu gizlilik politikası, kuruluşumuzun web sitemizi kullandığınızda sizden topladığımız kişisel verileri nasıl kullandığını açıklayacaktır.
+</p>
+
+<h3>Hangi verileri topluyoruz?</h3>
+<p>DISBOARD aşağıdaki verileri toplar:</p>
+<ul>
+    <li>
+        DISBOARD web sitesindeki aktivite günlükleri
+    </li>
+    <li>
+        E-posta adresi hariç Discord API'si tarafından sağlanan bilgiler
+    </li>
+</ul>
+
+<h3>Reşit Olmayanlar</h3>
+<p>
+Hizmetlerimiz 13 yaş ve üzeri kullanıcılar içindir ve bilerek 13 yaşın altındaki çocuklardan kişisel bilgi toplamayız. 13 yaşın altında bir çocuğun ebeveyni veya vasisi iseniz ve çocuğunuzun bize kişisel bilgilerini açıkladığını düşünüyorsanız, lütfen bizimle iletişime geçin. AEA sakinleri için, kişisel bilgilerin işlenmesinin rızaya dayalı olduğu durumlarda, DISBOARD bilerek bu işlemi geçerli veri koruma kanunu tarafından belirlenen rıza yaşının altındaki kullanıcılar için gerçekleştirmeyecektir. Bu tür kullanıcılarla bu işlemi yaptığımızı öğrenirsek, bu işlemi durduracak ve ilgili bilgileri kayıtlarımızdan kaldırmak için makul önlemler alacağız.
+</p>
+
+<h3>Verilerinizi nasıl topluyoruz?</h3>
+<p>
+    Topladığımız verilerin çoğunu doğrudan Şirketimize siz sağlarsınız. Aşağıdaki durumlarda veri toplar ve işleriz:
+</p>
+<ul>
+    <li>
+        Herhangi bir hizmetimize çevrimiçi kaydolduğunuzda.
+    </li>
+    <li>
+        Gönüllü olarak bir sunucu listesi, bir değerlendirme veya bir değerlendirmeye yanıt oluşturduğunuzda.
+    </li>
+    <li>
+        Tarayıcınızın çerezleri aracılığıyla web sitemizi kullandığınızda veya görüntülediğinizde.
+    </li>
+</ul>
+
+<h3>Verilerinizi nasıl kullanacağız?</h3>
+<p>
+    Aldığımız özel bilgileri yalnızca aşağıdaki amaçlar için kullanacağız:
+</p>
+<ul>
+    <li>
+        Hizmetimizi sağlamak için.
+    </li>
+    <li>
+        Ücretli hizmetimizi kullanan kullanıcılara ücret tahsil etmek için.
+    </li>
+    <li>
+        Bu hizmetin iyileştirilmesi veya yeni bir hizmetin eklenmesi için belirleyici veri olarak.
+    </li>
+    <li>
+        Bu hizmetin kullanım koşullarını ihlal eden kullanımı önlemek için.
+    </li>
+    <li>
+        Kullanıcıların kullanımı hakkında istatistiksel veri toplamak için (bu tür verilerin toplanması durumunda, veriler bir bireyi tanımlamayacak şekilde değiştirilecektir).
+    </li>
+    <li>
+        Kullanıcıların kullanım durumunu anlamak ve bunları bu hizmetin iyileştirilmesi ve yeni hizmetlerin geliştirilmesi için kullanmak için.
+    </li>
+    <li>
+        Sunduğumuz çeşitli üyelik hizmetleri ve diğer çeşitli hizmetler hakkında bilgi sağlamak için.
+    </li>
+</ul>
+
+<h3>Verilerinizi nasıl saklıyoruz?</h3>
+<p>
+Şirketimiz, verilerinizi üçüncü taraf barındırma tesislerinde güvenli bir şekilde saklar. <br>
+Şirketimiz, siz açıkça silinmesini talep edene kadar verilerinizi saklayacaktır.
+</p>
+
+<h3>Pazarlama</h3>
+<p>Verilerinizi pazarlama amaçlı olarak dağıtmıyoruz.</p>
+
+<h3>Veri koruma haklarınız nelerdir?</h3>
+<p>Şirketimiz, tüm veri koruma haklarınızın tamamen farkında olduğunuzdan emin olmak istiyor. Her kullanıcı aşağıdakilere hak kazanır:</p>
+<ul>
+    <?php /*
+    <li>
+        Erişim hakkı – Kişisel verilerinizin kopyalarını Şirketimizden talep etme hakkına sahipsiniz. Bu hizmet için sizden küçük bir ücret talep edebiliriz.
+    </li>
+    <li>
+        Düzeltme hakkı – Şirketimizden yanlış olduğunu düşündüğünüz bilgileri düzeltmesini talep etme hakkına sahipsiniz. Ayrıca, eksik olduğunu düşündüğünüz bilgileri Şirketimizin tamamlamasını talep etme hakkına da sahipsiniz.
+    </li>
+    */ ?>
+    <li>
+        Silme hakkı – Belirli koşullar altında, Şirketimizden kişisel verilerinizi silmesini talep etme hakkına sahipsiniz.
+    </li>
+    <li>
+        İşleme sınırlandırma hakkı – Belirli koşullar altında, Şirketimizden kişisel verilerinizin işlenmesini sınırlandırmasını talep etme hakkına sahipsiniz.
+    </li>
+    <?php /*
+    <li>
+        İşlemeye itiraz etme hakkı – Belirli koşullar altında, Şirketimizin kişisel verilerinizi işlemesine itiraz etme hakkına sahipsiniz.
+    </li>
+    <li>
+        Veri taşınabilirliği hakkı – Belirli koşullar altında, topladığımız verileri başka bir kuruluşa veya doğrudan size aktarmasını Şirketimizden talep etme hakkına sahipsiniz.
+    </li>
+    */ ?>
+    <li>
+        Bir talepte bulunursanız, size yanıt vermek için bir ayımız vardır. Bu haklardan herhangi birini kullanmak isterseniz, lütfen bizimle iletişime geçin.
+    </li>
+</ul>
+
+<h3>Çerezler</h3>
+<p>
+Çerezler, standart İnternet günlük bilgilerini ve ziyaretçi davranış bilgilerini toplamak için bilgisayarınıza yerleştirilen metin dosyalarıdır. Web sitemizi ziyaret ettiğinizde, çerezler veya benzer teknoloji aracılığıyla sizden otomatik olarak bilgi toplayabiliriz.
+Daha fazla bilgi için https://allaboutcookies.org adresini ziyaret edin.
+</p> 
